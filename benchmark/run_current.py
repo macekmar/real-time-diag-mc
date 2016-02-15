25993c31d45ea7c4d487287fe27c331ab9ec97de
Priyanka Seth <priyanka.seth@polytechnique.edu>
2016-02-15 18:37:23 +0100

    push

--- benchmark/run_current.py
+++ benchmark/run_current.py
@@ -12,7 +12,7 @@ n_cycles = 20000
 n_warmup_cycles = 100
 length_cycle = 1
 random_seed = 15258
-max_order = 3
+max_order = 2
 p_dbl = 0
 Nt_gf0 = 25000
 tmax_gf0 = 200.0
@@ -48,23 +48,23 @@ for order in range(0, max_order):
     print " pn = ", pn
     print " sn = ", sn
 
-#    inv_U_qmc = 1. / U_qmc
-#    cn_over_Zqmc = np.array([x * inv_U_qmc ** n for n, x in enumerate(pn)])
-#    fact = cn_over_Zqmc[-2] / c_norm if order > 0 else 1
-#    cn = cn_over_Zqmc / fact
-#    on = cn * sn
-#    on_list.append(on)
-#    cn_list.append(cn)
-#    on_last.append(on[-1])
-#    c_norm = cn[-1]  # for next iter
-#
-#    if mpi.rank == 0:
-#        print "----------- order = ", order, "---------------"
-#        print "cn = ", cn
-#        print "sn = ", sn
-#        print "on = ", on
-#        print "on_last = ", on_last
-#
-#if mpi.rank == 0:
-#    print "--------- on ---------"
-#    print on_list
+    inv_U_qmc = 1. / U_qmc
+    cn_over_Zqmc = np.array([x * inv_U_qmc ** n for n, x in enumerate(pn)])
+    fact = cn_over_Zqmc[-2] / c_norm if order > 0 else 1
+    cn = cn_over_Zqmc / fact
+    on = cn * sn
+    on_list.append(on)
+    cn_list.append(cn)
+    on_last.append(on[-1])
+    c_norm = cn[-1]  # for next iter
+
+    if mpi.rank == 0:
+        print "----------- order = ", order, "---------------"
+        print "cn = ", cn
+        print "sn = ", sn
+        print "on = ", on
+        print "on_last = ", on_last
+
+if mpi.rank == 0:
+    print "--------- on ---------"
+    print on_list
--- c++/qmc_data.hpp
+++ c++/qmc_data.hpp
@@ -39,19 +39,23 @@ struct g0_keldysh_t {
   // do not put alpha for the time_max even at equal time
   if (a == b) return g0_lesser(a.x, b.x, a.t, b.t) - ((b.t == t_max) ? 0_j : 1_j * alpha);
 
-  // mapping: is it lesser or greater ?
-  //  x    y    (x.time > y.time)   L/G ?
-  //  0    0           0             L
-  //  0    0           1             G
-  //  1    1           0             G
-  //  1    1           1             L
-  //
-  //  0    1           *            L
-  //  1    0           *            G
-  bool is_lesser = (a.k_index == b.k_index ? (a.k_index xor (a.t > b.t)) : b.k_index);
-  return (is_lesser ? g0_lesser(a.x, b.x, a.t, b.t) : g0_greater(a.x, b.x, a.t, b.t));
+//  // mapping: is it lesser or greater?
+//  //  x    y    (x.time > y.time)   L/G ?
+//  //  0    0           0             L
+//  //  0    0           1             G
+//  //  1    1           0             G
+//  //  1    1           1             L
+//  //
+//  //  0    1           *            L
+//  //  1    0           *            G
+//  bool is_lesser = (a.k_index == b.k_index ? (a.k_index xor (a.t > b.t)) : b.k_index);
+//  return (is_lesser ? g0_lesser(a.x, b.x, a.t, b.t) : g0_greater(a.x, b.x, a.t, b.t));
 
   // old code. slower because more tests FIXME: REMOVE AFTER DEBUG
+   if (a.k_index == 0 && b.k_index == 0) return (a.t > b.t ? g0_greater(a.x, b.x, a.t, b.t) : g0_lesser(a.x, b.x, a.t, b.t));
+   if (a.k_index == 1 && b.k_index == 1) return (a.t > b.t ? g0_lesser(a.x, b.x, a.t, b.t) : g0_greater(a.x, b.x, a.t, b.t));
+   if (a.k_index == 1 && b.k_index == 0) return g0_greater(a.x, b.x, a.t, b.t);
+   return g0_lesser(a.x, b.x, a.t, b.t);
   // if (x.k_index == 0 && y.k_index == 0) return (x.time > y.time ? g0_greater(dr, dt) : g0_lesser(dr, dt));
   // if (x.k_index == 1 && y.k_index == 1) return (x.time > y.time ? g0_lesser(dr, dt) : g0_greater(dr, dt));
   // if (x.k_index == 1 && y.k_index == 0) return g0_greater(dr, dt);
