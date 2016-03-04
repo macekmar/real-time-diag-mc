7d56633d3805402dcc499f00680d44262d1b0c20
Antoine Maillard <antoine.maillard@ens.fr>
2016-03-04 14:05:35 +0100

    Adding histograms fior measures instead of observables

--- c++/measures.hpp
+++ c++/measures.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <triqs/mc_tools.hpp>
 #include <triqs/statistics.hpp>
+#include <triqs/statistics/histograms.hpp>
 #include <triqs/clef.hpp>
 #include <triqs/arrays.hpp>
 #include "./qmc_data.hpp"
@@ -18,44 +19,54 @@ struct measure_pn_sn {
  array<double, 1> &sn_errors;
  int size_n;
 
- // Creating an array of observables
- array<observable<double>, 1> observable_pn;
- array<observable<double>, 1> observable_sn;
+ 
+ //Creating an histogram
+ histogram histogram_pn;
+ histogram histogram_sn;
 
 // ---------- 
  measure_pn_sn(qmc_data_t* data, array<double, 1>* pn, array<double, 1>* sn, array<double, 1>* pn_errors,
                array<double, 1>* sn_errors)
     : data(data), pn(*pn), sn(*sn), pn_errors(*pn_errors), sn_errors(*sn_errors) {
   size_n = first_dim(*pn);
-  observable_pn.resize(size_n);
-  observable_sn.resize(size_n);
+  histogram_pn = histogram(0,size_n-1);
+  histogram_sn = histogram(0,size_n-1);
  }
 
 // ---------- 
  void accumulate(dcomplex sign) {
 
   int k = data->perturbation_order;
-  // Accumulating in pn
-  for (int i = 0; i < size_n; i++) observable_pn(i) << ((i == k) ? 1 : 0);
-  // Accumulating in sn
-  observable_sn(k) << real(sign);
+
+  histogram_pn << k;
+  //we only keep the number of + signs, then the number of - signs is just pn(i) - number(+ signs)
+  if(real(sign) > 0) histogram_sn << k;
  }
 
 // ---------- 
  void collect_results(mpi::communicator c) {
 
-  // Putting the values from all cores together
-  for (int i = 0; i < size_n; i++) {
-   auto&& series_pn = observable_pn(i).get_series();
-   auto&& series_sn = observable_sn(i).get_series();
 
-  // Now we treat the data to get the correct average values and errors.
-   auto aver_and_err_pn = average_and_error(observable<double>(mpi_all_gather(series_pn, c)));
-   auto aver_and_err_sn = average_and_error(observable<double>(mpi_all_gather(series_sn, c)));
-   pn(i) = aver_and_err_pn.value;
-   sn(i) = aver_and_err_sn.value;
-   pn_errors(i) = aver_and_err_pn.error_bar;
-   sn_errors(i) = aver_and_err_sn.error_bar;
+   //We do it with the histogram class
+   histogram histogram_pn_full = mpi_reduce(histogram_pn,c);
+   histogram histogram_sn_full = mpi_reduce(histogram_sn,c);
+   auto data_histogram_pn = histogram_pn_full.data();
+   auto data_histogram_sn = histogram_sn_full.data();
+   int number_points = histogram_pn_full.n_data_pts();
+   //Computing the average and error values
+   //For pn
+   for (int i = 0; i < size_n ; i++)
+   {
+           pn(i) = data_histogram_pn(i)/number_points; //Average
+           sn(i) = 2*(data_histogram_sn(i)/(data_histogram_pn(i))) - 1;
+
+           //FIXME : explicit formula for the error bar jacknife of a series of 0 and 1
+           pn_errors(i) = (1/double(pow(number_points,2))) * sqrt((number_points - 1) * data_histogram_pn(i) * (number_points - data_histogram_pn(i)));
+           
+           //FIXME : explicit formula as well for the error bar of the sn using a jacknife
+           sn_errors(i) = (2/double(pow(data_histogram_pn(i),2))) * sqrt((data_histogram_pn(i) - 1) * data_histogram_sn(i)* (data_histogram_pn(i) - data_histogram_sn(i)));
+            
+            
   }
  }
 };
