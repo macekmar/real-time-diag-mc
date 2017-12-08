d82b2bbb6fb7b338d19052a8693f64dd23f95e19
Corentin B <corentin.bertrand@cea.fr>
2017-12-08 19:38:10 +0100

    New test for Configuration in the 2-particle case

--- test/c++/configuration_2.cpp
+++ test/c++/configuration_2.cpp
@@ -4,7 +4,7 @@
 using namespace triqs::gfs;
 using triqs::arrays::range;
 
-#define ABS_TOL 1.e-16
+#define ABS_TOL 1.e-15
 #define REL_TOL 1.e-10
 bool is_not_close(dcomplex a, dcomplex b) {
  bool output = abs(a - b) > REL_TOL * abs(b) + ABS_TOL;
@@ -12,7 +12,9 @@ bool is_not_close(dcomplex a, dcomplex b) {
  return output;
 }
 bool is_not_close_array1(array<dcomplex, 1> a, array<dcomplex, 1> b) {
- return max_element(abs(a - b) - REL_TOL * abs(b)) > ABS_TOL;
+ bool output = max_element(abs(a - b) - REL_TOL * abs(b)) > ABS_TOL;
+ if (output) std::cout << "Diff = " << max_element(abs(a - b)) << std::endl;
+ return output;
 }
 bool is_close_array2(array<dcomplex, 2> a, array<dcomplex, 2> b) {
  return max_element(abs(a - b) - REL_TOL * abs(b)) > ABS_TOL;
@@ -41,10 +43,12 @@ int main() {
  auto g_grea = conj(g_less);
 
  auto g0 = g0_keldysh_t{g_less, g_grea};
- std::vector<dcomplex> alphas{0.2};
+ std::vector<dcomplex> alphas{0., 1.5};
  g0_keldysh_alpha_t g0_alpha = g0_keldysh_alpha_t{g0, 0.5, alphas};
 
- auto a = keldysh_contour_pt{0, 1.5, 0, 0};
+ auto a_up = keldysh_contour_pt{0, 1.5, 0, 0};
+ auto a_up_p = keldysh_contour_pt{0, 1.4, 1, 0};
+ auto a_do = keldysh_contour_pt{1, 0.5, 0, 1};
  auto b0 = keldysh_contour_pt{0, 0.3, 0};
  auto b1 = flip_index(b0);
  auto c0 = keldysh_contour_pt{0, -1.3, 0};
@@ -52,15 +56,12 @@ int main() {
  auto d0 = keldysh_contour_pt{0, 0.65, 0};
  auto d1 = flip_index(d0);
 
- std::vector<keldysh_contour_pt> an_pts{
-     a,
- };
- std::vector<keldysh_contour_pt> cr_pts{
-     a,
- };
+ std::vector<keldysh_contour_pt> an_pts{a_up, a_do};
+ std::vector<keldysh_contour_pt> cr_pts{a_up_p, a_do};
+ a_do.x = 0;
 
-  auto sing_th = std::pair<double, double>{3.5, 3.5};
- //auto sing_th = std::pair<double, double>{-10000, -10000};
+ auto sing_th = std::pair<double, double>{3.5, 3.5};
+ // auto sing_th = std::pair<double, double>{-10000, -10000};
 
  Configuration config(g0_alpha, an_pts, cr_pts, 4, sing_th, true);
  dcomplex ref_weight = 1;
@@ -78,9 +79,12 @@ int main() {
 
  config.print();
 
- ref_kernels(0, 0) = -g0_alpha(b0, b0) * g0_alpha(b0, a);
- ref_kernels(0, 1) = g0_alpha(b1, b1) * g0_alpha(b1, a);
- ref_weight = std::abs(ref_kernels(0, 0)) + std::abs(ref_kernels(0, 1));
+ ref_kernels(0, 0) = -g0_alpha(b0, a_up_p) * det_2x2(g0_alpha, b0, a_do, b0, a_do);
+ ref_kernels(0, 1) = +g0_alpha(b1, a_up_p) * det_2x2(g0_alpha, b1, a_do, b1, a_do);
+ ref_kernels(1, 1) = +g0_alpha(b0, b0) * det_2x2(g0_alpha, b0, a_do, b0, a_do); // only index of a_up_p (1)
+ ref_kernels(1, 1) += -g0_alpha(b1, b1) * det_2x2(g0_alpha, b1, a_do, b1, a_do);
+ ref_weight = sum(abs(ref_kernels(0, range())));
+ ref_weight += sum(abs(ref_kernels(1, range())));
 
  std::cout << ref_weight << std::endl;
  std::cout << config.current_weight << std::endl;
@@ -100,7 +104,8 @@ int main() {
 
  if (config.current_weight != 1.) return 30;
  if (is_not_close(config.accepted_weight, ref_weight)) return 31; // has not changed
- if (is_not_close_array1(config.accepted_kernels(0, range()), ref_kernels(0, range()))) return 32; // has not changed
+ if (is_not_close_array1(config.accepted_kernels(0, range()), ref_kernels(0, range())))
+  return 32; // has not changed
  if (config.order != 0) return 33;
 
  // add two points and accept;
@@ -112,15 +117,21 @@ int main() {
 
  ref_kernels() = 0;
 
- ref_kernels(0, 0) += det_2x2(g0_alpha, c0, d0, d0, a) * det_2x2(g0_alpha, c0, d0, c0, d0);
- ref_kernels(0, 0) -= det_2x2(g0_alpha, c0, d1, d1, a) * det_2x2(g0_alpha, c0, d1, c0, d1);
- ref_kernels(0, 1) -= det_2x2(g0_alpha, c1, d0, d0, a) * det_2x2(g0_alpha, c1, d0, c1, d0);
- ref_kernels(0, 1) += det_2x2(g0_alpha, c1, d1, d1, a) * det_2x2(g0_alpha, c1, d1, c1, d1);
+ ref_kernels(0, 0) += det_2x2(g0_alpha, c0, d0, d0, a_up_p) * det_3x3(g0_alpha, c0, d0, a_do, c0, d0, a_do);
+ ref_kernels(0, 0) -= det_2x2(g0_alpha, c0, d1, d1, a_up_p) * det_3x3(g0_alpha, c0, d1, a_do, c0, d1, a_do);
+ ref_kernels(0, 1) -= det_2x2(g0_alpha, c1, d0, d0, a_up_p) * det_3x3(g0_alpha, c1, d0, a_do, c1, d0, a_do);
+ ref_kernels(0, 1) += det_2x2(g0_alpha, c1, d1, d1, a_up_p) * det_3x3(g0_alpha, c1, d1, a_do, c1, d1, a_do);
 
- ref_kernels(1, 0) -= det_2x2(g0_alpha, c0, d0, c0, a) * det_2x2(g0_alpha, c0, d0, c0, d0);
- ref_kernels(1, 0) += det_2x2(g0_alpha, c1, d0, c1, a) * det_2x2(g0_alpha, c1, d0, c1, d0);
- ref_kernels(1, 1) += det_2x2(g0_alpha, c0, d1, c0, a) * det_2x2(g0_alpha, c0, d1, c0, d1);
- ref_kernels(1, 1) -= det_2x2(g0_alpha, c1, d1, c1, a) * det_2x2(g0_alpha, c1, d1, c1, d1);
+ ref_kernels(1, 0) -= det_2x2(g0_alpha, c0, d0, c0, a_up_p) * det_3x3(g0_alpha, c0, d0, a_do, c0, d0, a_do);
+ ref_kernels(1, 0) += det_2x2(g0_alpha, c1, d0, c1, a_up_p) * det_3x3(g0_alpha, c1, d0, a_do, c1, d0, a_do);
+ ref_kernels(1, 1) += det_2x2(g0_alpha, c0, d1, c0, a_up_p) * det_3x3(g0_alpha, c0, d1, a_do, c0, d1, a_do);
+ ref_kernels(1, 1) -= det_2x2(g0_alpha, c1, d1, c1, a_up_p) * det_3x3(g0_alpha, c1, d1, a_do, c1, d1, a_do);
+
+ // only index of a_up_p (1)
+ ref_kernels(2, 1) += det_2x2(g0_alpha, c0, d0, c0, d0) * det_3x3(g0_alpha, c0, d0, a_do, c0, d0, a_do);
+ ref_kernels(2, 1) -= det_2x2(g0_alpha, c1, d0, c1, d0) * det_3x3(g0_alpha, c1, d0, a_do, c1, d0, a_do);
+ ref_kernels(2, 1) -= det_2x2(g0_alpha, c0, d1, c0, d1) * det_3x3(g0_alpha, c0, d1, a_do, c0, d1, a_do);
+ ref_kernels(2, 1) += det_2x2(g0_alpha, c1, d1, c1, d1) * det_3x3(g0_alpha, c1, d1, a_do, c1, d1, a_do);
 
  ref_weight = sum(abs(ref_kernels));
 
@@ -133,7 +144,8 @@ int main() {
  if (is_not_close(config.current_weight, ref_weight)) return 40;
  if (is_not_close_array1(config.current_kernels(0, range()), ref_kernels(0, range()))) return 41;
  if (is_not_close_array1(config.current_kernels(1, range()), ref_kernels(1, range()))) return 42;
- if (config.order != 2) return 43;
+ if (is_not_close_array1(config.current_kernels(2, range()), ref_kernels(2, range()))) return 43;
+ if (config.order != 2) return 44;
 
  std::cout << "success" << std::endl;
  return 0;
