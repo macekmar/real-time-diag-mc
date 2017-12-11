32c87b2fa947901c6c01aedda4c196e286d70420
Corentin B <corentin.bertrand@cea.fr>
2017-12-11 14:19:29 +0100

    Another test for configuration

--- test/c++/cofactor_row_1.cpp
+++ test/c++/cofactor_row_1.cpp
@@ -3,39 +3,59 @@
 class Function {
  public:
  Function() {};
- double operator()(const int& i, const int& j) const {return i+j*j;};
+ double operator()(const int& i, const int& j) const {return i-3+j*j;};
+};
+
+bool are_equal_det_manip(det_manip<Function> m1, det_manip<Function> m2) {
+ if (m1.size() != m2.size()) return false;
+ bool output = true;
+ for (int i=0; i<m1.size(); ++i) {
+  output = output and m1.get_x(i) == m2.get_x(i) and m1.get_y(i) == m2.get_y(i);
+ }
+ return output;
+};
+
+void print_det_manip(det_manip<Function> m) {
+ for (int i=0; i<m.size(); ++i) std::cout << m.get_x(i) << ", ";
+ std::cout << std::endl;
+ for (int i=0; i<m.size(); ++i) std::cout << m.get_y(i) << ", ";
+ std::cout << std::endl;
 };
 
 int main() {
 
  Function f;
  auto matrix = det_manip<Function>(f, 10);
+ auto matrix_ref = det_manip<Function>(f, 10);
 
  for (int i=0; i<3; ++i) {
-  matrix.insert_at_end(i, i);
+  matrix.insert_at_end(i+3, i);
+  matrix_ref.insert_at_end(i+3, i);
  }
 
- // This matrix is stricly NOT invertible. `cofactor_row` must work in this case.
- auto matrix_save = matrix.matrix();
- //std::cout << matrix.matrix() << std::endl;
- //std::cout << matrix.inverse_matrix() << std::endl;
+ // This matrix is stricly NOT invertible. `cofactor_row` has to work in this case.
+ std::cout << matrix.matrix() << std::endl;
+ std::cout << matrix.inverse_matrix() << std::endl;
 
  // test n=2
  matrix.regenerate();
  auto cofactors = cofactor_row(matrix, 1, 2);
- //std::cout << cofactors << std::endl;
- if (not (cofactors.rank == 1 and first_dim(cofactors) == 2)) return 1;
- if (not (cofactors(0) == 6. and cofactors(1) == -8.)) return 2;
- if (matrix.matrix() != matrix_save) return 3;
+ print_det_manip(matrix);
+ std::cout << cofactors << std::endl;
+ if (not are_equal_det_manip(matrix, matrix_ref)) return 10;
+ if (not (cofactors.rank == 1 and first_dim(cofactors) == 2)) return 11;
+ if (not (cofactors(0) == 6. and cofactors(1) == -8.)) return 12;
 
  // test n=3
  matrix.regenerate();
  cofactors = cofactor_row(matrix, 1, 3);
- //std::cout << cofactors << std::endl;
- if (not (cofactors.rank == 1 and first_dim(cofactors) == 3)) return 4;
- if (not (cofactors(0) == 6. and cofactors(1) == -8. and cofactors(2) == 2.)) return 5;
- if (matrix.matrix() != matrix_save) return 6;
+ print_det_manip(matrix);
+ std::cout << cofactors << std::endl;
+ if (not are_equal_det_manip(matrix, matrix_ref)) return 20;
+ if (not (cofactors.rank == 1 and first_dim(cofactors) == 3)) return 21;
+ if (not (cofactors(0) == 6. and cofactors(1) == -8. and cofactors(2) == 2.)) return 22;
 
+ std::cout << "success" << std::endl;
  return 0;
 }
 
---
+++ test/c++/configuration_3.cpp
@@ -0,0 +1,108 @@
+#include "../c++/configuration.hpp"
+#include <triqs/gfs.hpp>
+
+using namespace triqs::gfs;
+using triqs::arrays::range;
+
+bool are_equal_pts(keldysh_contour_pt const& p1, keldysh_contour_pt const& p2) {
+ return p1.x == p2.x and p1.t == p2.t and p1.k_index == p2.k_index and p1.rank == p2.rank;
+};
+
+int main() {
+ /// Test that the evaluation of configurations does not change the matrices.
+
+ auto g_less = gf<retime, matrix_valued>{{-10., 10., 1001}, {2, 2}};
+ auto g_less_f = make_gf_from_fourier(g_less);
+ triqs::clef::placeholder<0> w_;
+ g_less_f(w_) << -0.5_j / (w_ + 1_j);
+ g_less = make_gf_from_inverse_fourier(g_less_f);
+ auto g_grea = conj(g_less);
+
+ auto g0 = g0_keldysh_t{g_less, g_grea};
+ std::vector<dcomplex> alphas{0., 0.5};
+ g0_keldysh_alpha_t g0_alpha = g0_keldysh_alpha_t{g0, 0.5, alphas};
+
+ auto a_up = keldysh_contour_pt{0, 0.0, 0, 0};
+ auto a_up_p = keldysh_contour_pt{0, 0.1, 1, 0};
+ auto a_do = keldysh_contour_pt{1, 0.2, 0, 1};
+ auto a_do_p = keldysh_contour_pt{1, 0.3, 0, 1};
+ auto b = keldysh_contour_pt{0, 0.4, 0};
+ auto c = keldysh_contour_pt{0, 0.5, 0};
+ auto d = keldysh_contour_pt{0, 0.6, 0};
+
+ std::vector<keldysh_contour_pt> an_pts{a_up, a_do};
+ std::vector<keldysh_contour_pt> cr_pts{a_up_p, a_do_p};
+ a_do.x = 0;
+ a_do_p.x = 0;
+
+ auto sing_th = std::pair<double, double>{3.5, 3.5};
+ //auto sing_th = std::pair<double, double>{-10000, -10000};
+
+ Configuration config(g0_alpha, an_pts, cr_pts, 4, sing_th, true);
+
+ if (config.matrices[0].size() != 1) return 10;
+ if (not are_equal_pts(config.matrices[0].get_x(0), a_up)) return 11;
+ if (not are_equal_pts(config.matrices[0].get_y(0), a_up_p)) return 12;
+ if (config.matrices[1].size() != 1) return 13;
+ if (not are_equal_pts(config.matrices[1].get_x(0), a_do)) return 14;
+ if (not are_equal_pts(config.matrices[1].get_y(0), a_do_p)) return 15;
+
+ // add a point and accept the config
+ config.insert(0, b);
+ config.evaluate();
+ config.accept_config();
+
+ config.print();
+
+ if (config.matrices[0].size() != 2) return 20;
+ if (not are_equal_pts(config.matrices[0].get_x(0), b)) return 21;
+ if (not are_equal_pts(config.matrices[0].get_y(0), b)) return 22;
+ if (not are_equal_pts(config.matrices[0].get_x(1), a_up)) return 23;
+ if (not are_equal_pts(config.matrices[0].get_y(1), a_up_p)) return 24;
+ if (config.matrices[1].size() != 2) return 25;
+ if (not are_equal_pts(config.matrices[1].get_x(0), b)) return 26;
+ if (not are_equal_pts(config.matrices[1].get_y(0), b)) return 27;
+ if (not are_equal_pts(config.matrices[1].get_x(1), a_do)) return 28;
+ if (not are_equal_pts(config.matrices[1].get_y(1), a_do_p)) return 29;
+
+ // remove point and do not accept
+ config.remove(0);
+ config.evaluate();
+
+ config.print();
+
+ if (config.matrices[0].size() != 1) return 30;
+ if (not are_equal_pts(config.matrices[0].get_x(0), a_up)) return 31;
+ if (not are_equal_pts(config.matrices[0].get_y(0), a_up_p)) return 32;
+ if (config.matrices[1].size() != 1) return 33;
+ if (not are_equal_pts(config.matrices[1].get_x(0), a_do)) return 34;
+ if (not are_equal_pts(config.matrices[1].get_y(0), a_do_p)) return 35;
+
+ // add two points and accept;
+ config.insert2(0, 1, c, d);
+ config.evaluate();
+ config.accept_config();
+
+ config.print();
+
+ if (config.matrices[0].size() != 3) return 40;
+ if (not are_equal_pts(config.matrices[0].get_x(0), c)) return 41;
+ if (not are_equal_pts(config.matrices[0].get_y(0), c)) return 42;
+ if (not are_equal_pts(config.matrices[0].get_x(1), d)) return 43;
+ if (not are_equal_pts(config.matrices[0].get_y(1), d)) return 44;
+ if (not are_equal_pts(config.matrices[0].get_x(2), a_up)) return 45;
+ if (not are_equal_pts(config.matrices[0].get_y(2), a_up_p)) return 46;
+
+ if (config.matrices[1].size() != 3) return 50;
+ if (not are_equal_pts(config.matrices[1].get_x(0), c)) return 51;
+ if (not are_equal_pts(config.matrices[1].get_y(0), c)) return 52;
+ if (not are_equal_pts(config.matrices[1].get_x(1), d)) return 53;
+ if (not are_equal_pts(config.matrices[1].get_y(1), d)) return 54;
+ if (not are_equal_pts(config.matrices[1].get_x(2), a_do)) return 55;
+ if (not are_equal_pts(config.matrices[1].get_y(2), a_do_p)) return 56;
+
+ std::cout << "nb cofactors = " << config.nb_cofact << std::endl;
+ std::cout << "nb inverse = " << config.nb_inverse << std::endl;
+ std::cout << "success" << std::endl;
+ return 0;
+}
