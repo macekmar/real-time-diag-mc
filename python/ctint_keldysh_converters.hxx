8f3751bef90232b967c6ee52cfb137936882752e
Marjan Macek <macek.marjan@hotmail.com>
2019-01-16 17:13:31 +0100

    Update py/c++ interface files to include AuxMC parameters

--- python/ctint_keldysh_converters.hxx
+++ python/ctint_keldysh_converters.hxx
@@ -39,6 +39,8 @@ template <> struct py_converter<solve_parameters_t> {
   PyDict_SetItemString( d, "store_configurations"  , convert_to_python(x.store_configurations));
   PyDict_SetItemString( d, "preferential_sampling" , convert_to_python(x.preferential_sampling));
   PyDict_SetItemString( d, "ps_gamma"              , convert_to_python(x.ps_gamma));
+  PyDict_SetItemString( d, "do_aux_mc"             , convert_to_python(x.do_aux_mc));
+  PyDict_SetItemString( d, "nb_aux_mc_cycles"      , convert_to_python(x.nb_aux_mc_cycles));
   return d;
  }
 
@@ -84,6 +86,8 @@ template <> struct py_converter<solve_parameters_t> {
   _get_optional(dic, "store_configurations"  , res.store_configurations     ,0);
   _get_optional(dic, "preferential_sampling" , res.preferential_sampling    ,false);
   _get_optional(dic, "ps_gamma"              , res.ps_gamma                 ,1.);
+  _get_optional(dic, "do_aux_mc"             , res.do_aux_mc                ,false);
+  _get_optional(dic, "nb_aux_mc_cycles"      , res.nb_aux_mc_cycles         ,5);
   return res;
  }
 
@@ -114,7 +118,7 @@ template <> struct py_converter<solve_parameters_t> {
   std::stringstream fs, fs2; int err=0;
 
 #ifndef TRIQS_ALLOW_UNUSED_PARAMETERS
-  std::vector<std::string> ks, all_keys = {"creation_ops","annihilation_ops","extern_alphas","nonfixed_op","interaction_start","alpha","nb_orbitals","potential","U","w_ins_rem","w_dbl","w_shift","max_perturbation_order","min_perturbation_order","forbid_parity_order","length_cycle","random_seed","random_name","verbosity","method","nb_bins","singular_thresholds","cycles_trapped_thresh","store_configurations","preferential_sampling","ps_gamma"};
+  std::vector<std::string> ks, all_keys = {"creation_ops","annihilation_ops","extern_alphas","nonfixed_op","interaction_start","alpha","nb_orbitals","potential","U","w_ins_rem","w_dbl","w_shift","max_perturbation_order","min_perturbation_order","forbid_parity_order","length_cycle","random_seed","random_name","verbosity","method","nb_bins","singular_thresholds","cycles_trapped_thresh","store_configurations","preferential_sampling","ps_gamma","do_aux_mc", "nb_aux_mc_cycles"};
   pyref keys = PyDict_Keys(dic);
   if (!convertible_from_python<std::vector<std::string>>(keys, true)) {
    fs << "\nThe dict keys are not strings";
@@ -152,6 +156,8 @@ template <> struct py_converter<solve_parameters_t> {
   _check_optional <int                                                                             >(dic, fs, err, "store_configurations"  , "int");
   _check_optional <bool                                                                            >(dic, fs, err, "preferential_sampling" , "bool");
   _check_optional <double                                                                          >(dic, fs, err, "ps_gamma"              , "double");
+  _check_optional <bool                                                                          >(dic, fs, err, "do_aux_mc"              , "bool");
+  _check_optional <int                                                                          >(dic, fs, err, "nb_aux_mc_cycles"              , "int");
   if (err) goto _error;
   return true;
 
