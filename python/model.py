Add calculation at fixed N

python/model.py
@@ -52,10 +52,11 @@ def generate_u(inv_cdf, interaction_start, N_samples, dim, N_skip=0):
    
 def generate_u_complex(inv_cdf, interaction_start, N_samples, dim, N_skip=0):
     """Generates u distributed by spring model.
-    It returns N_samples in the domain"""
-     # This came from a fit
+    It returns u points where at least N_samples are in the domain."""
+     # This came from a fit for Corentin 2 parameters !!!!!!!!!!!!!!!!!!!
     N_batch = int(1.02/np.exp(-(dim-1.1325)**2/7.6**2)*N_samples)
     itr = 0
+    N = 0
     while N < N_samples:
         # Inverse transform sampling
         v = sobol_seq.i4_sobol_generate(dim, N_batch, skip=N_skip + N+1)
@@ -68,9 +69,9 @@ def generate_u_complex(inv_cdf, interaction_start, N_samples, dim, N_skip=0):
         # Reject u out of t_min
         inds = np.where(np.all(u > interaction_start, axis=1))[0]
         if N == 0:
-            u_samples = u[inds,:]
+            u_samples = u#[inds,:]
         else:
-            u_samples = np.vstack((u_samples, u[inds,:]))
+            u_samples = np.vstack((u_samples, u)) #[inds,:]))
 
         N += len(inds)
 
@@ -78,18 +79,19 @@ def generate_u_complex(inv_cdf, interaction_start, N_samples, dim, N_skip=0):
         #print("Loop %d, added: %d, done %d" % (itr, len(inds), N_batch))
         N_batch = int(1.02/np.exp(-(dim-1.1325)**2/7.65**2)*(N_samples-N))
     # At most we reject 10 % of samples!
-    return u_samples[:N_samples,:]
+    return u_samples #[:N_samples,:]
 
 
-def _calculate_inv_cdf(fun, t_min, t_max=0, Nt=201):
+def _calculate_inv_cdf(fun, t_min, t_max=0, Nt=1001):
     """Calculates inverse CDF for a nonnormalized function."""
     u_lin = np.linspace(t_min, t_max, Nt)
     fun_val = np.abs(fun(u_lin[:, np.newaxis])) # Newaxis is necessary for the get function
     cdf = cumtrapz(fun_val, u_lin, initial=0)
-    cdf = cdf/cdf[-1]
-    return PchipInterpolator(cdf, u_lin)
+    integral = cdf[-1]
+    cdf = cdf/integral
+    return integral, PchipInterpolator(cdf, u_lin)
 
-def calculate_inv_cdfs(model, t_min, t_max=0, Nt=201):
+def calculate_inv_cdfs(model, t_min, t_max=0, Nt=1001):
     
     world = MPI.COMM_WORLD
     # Split
@@ -99,15 +101,17 @@ def calculate_inv_cdfs(model, t_min, t_max=0, Nt=201):
         funs = np.array_split(funs, world.size)
     else:
         funs = None
-
     funs = world.scatter(funs, root=0)
+    integral = [None for i in range(len(funs))]
     inv_cdf = [None for i in range(len(funs))]
     # Calculate
     for i, f in enumerate(funs):
-        inv_cdf[i] = _calculate_inv_cdf(f, t_min=t_min, t_max=t_max, Nt=Nt)
+        integral[i], inv_cdf[i] = _calculate_inv_cdf(f, t_min=t_min, t_max=t_max, Nt=Nt)
     # Gather
+    integral = world.gather(integral, root=0)
     inv_cdf = world.gather(inv_cdf, root=0)
     if world.rank == 0:
+        integral = [l for arr in integral for l in arr]
         inv_cdf = [l for arr in inv_cdf for l in arr]
 
-    return inv_cdf
+    return integral, inv_cdf

python/quasimc.py
@@ -4,7 +4,7 @@ from time import clock
 import numpy as np
 
 import model
-from quasimc_utility import distribute_u
+from quasimc_utility import distribute_u, distribute_u_complex
 
 from ctint_keldysh import SolverCore
 from mpi4py import MPI
@@ -47,8 +47,7 @@ def quasi_solver(solver, **params):
     _add_params_to_results(results_to_save, dict(params_cpp, **params_py))
 
     ### Calculate inverse CDFs
-    inv_cdf = model.calculate_inv_cdfs(model_funs, t_min=t_min)
-
+    integral, inv_cdf = model.calculate_inv_cdfs(model_funs, t_min=t_min)
 
     N_vec = [0] + params_py['N'][:]
     N_max = N_vec[-1]
@@ -58,17 +57,28 @@ def quasi_solver(solver, **params):
 
     ### Calculation
     for io, order in enumerate(orders):
+
+        if world.rank == 0:
+            print("")
+            print("==================================================================")
+            print("Order: {0}".format(order))
+            print("")
+
         results = {}
         results['results_part'] = {}
 
+
         ### Generate quasi random numbers
+        points = [None for i in range(len(N_vec)-1)]
+        N_true = [0 for i in range(len(N_vec)-1)]
         if world.rank == 0:
             print('Generating quasi random numbers')
-            u = model.generate_u(inv_cdf, t_min, N_max, order)
-            N_true, points = distribute_u(u, order, t_min, N_vec, world.size)
-        else:
-            points = [None for i in range(len(N_vec)-1)]
-            N_true = [0 for i in range(len(N_vec)-1)]
+            # u = model.generate_u(inv_cdf, t_min, N_max, order)
+            # N_true, points = distribute_u(u, order, t_min, N_vec, world.size)
+
+            # This generates roughly enough points
+            u = model.generate_u_complex(inv_cdf, t_min, N_max, order)
+            N_true, points = distribute_u_complex(u, order, t_min, N_vec, world.size)
 
         N_true = world.bcast(N_true, root=0)
 
@@ -77,7 +87,8 @@ def quasi_solver(solver, **params):
         for iN in range(len(N_vec) - 1):
             if world.rank == 0:
                 print("Calculating points from {0} to {1}.".format(N_vec[iN], N_vec[iN+1]))
-                print("Number of points in the domain: {0}".format({N_true[iN]}))
+                print("Number of generated points: {0}".format(N_true[iN]))
+                print("Number of points in the domain (+ trailing zeros): {0}".format(np.sum([p.shape[0] for p in points[iN]])))
 
 
             u = world.scatter(points[iN], root=0)
@@ -86,6 +97,9 @@ def quasi_solver(solver, **params):
             u = u[inds]
 
             # Calculate
+
+            # TODO: Implement 'save_period'
+
             _ = model.get(solver, u, True)
             N_total += N_true[iN]
 
@@ -143,8 +157,13 @@ def quasi_solver(solver, **params):
                 if io == 0 and iN == 0:
                     for key in results:
                         results_to_save[key] = results[key]
-                    results_to_save["results_all"]["cn"] = np.ones(params_cpp["max_perturbation_order"]+1)
-                    results_to_save["results_part"]["cn"] = np.ones((params_cpp["max_perturbation_order"]+1, iN+1))
+                    cn = [np.prod(integral[-i-1:]) for i in range(len(integral))]
+                    cn = np.insert(cn, 0, 1)
+                    # results_to_save["results_all"]["cn"] = np.ones(params_cpp["max_perturbation_order"]+1)
+                    # results_to_save["results_part"]["cn"] = np.ones((params_cpp["max_perturbation_order"]+1, iN+1))
+                    results_to_save["results_all"]["cn"] = cn
+                    #results_to_save["results_all"]["cn"][1:] /= results_to_save["results_all"]["pn"][0]
+                    results_to_save["results_part"]["cn"] = cn[:, np.newaxis] #np.tile(cn, (len(N_vec), 1))
                 else:
                     results_to_save = quasi_merge_results(order, results_to_save, results)
 